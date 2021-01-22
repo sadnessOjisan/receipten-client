99a772f6ef955517093dfb9e6da659dae1d656da
sadness_ojisan <sadness.ojisan@gmail.com>
2021-01-23 03:40:32 +0900

    wip

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ miniserve ./static --index index.html
 # build
 wasm-pack build --target web --out-name wasm --out-dir ./static
 ```
+
+## TODO
+
+- [ ] routing から id 取得
+- [ ] id を使ってデータ取得
+- [ ] データを UI に表示
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,31 @@
 use wasm_bindgen::prelude::*;
-use yew::prelude::*;
+use yew::{format::{Json, Nothing}, prelude::*, services::{fetch::{self, FetchTask, Request, Response}}};
+use yew::services::fetch::{FetchService};
 
 struct Model {
     // リンクコンポーネント -> コンポーネントがコールバックを登録できて自身を更新することができるメカニズム
+    fetch_service: FetchService,
     link: ComponentLink<Self>,
     value: i64,
+    fetching: bool,
+    data: Option<u32>,
+    ft: Option<FetchTask>,
 }
 
 enum Msg {
-    AddOne,
+    AddOne,  FetchData,
+    FetchReady(Result<Item, Error>), // 2.
+    Ignore,
 }
+
+struct Item {
+    itemName: String,
+    itemPrice: i32,
+    fetching: bool,
+    data: Option<u32>,
+    ft: Option<FetchTask>,
+}
+
 
 impl Component for Model {
 
@@ -26,13 +42,41 @@ impl Component for Model {
         Self {
             link,
             value: 0,
+            fetch_service: FetchService::default(),
+            fetching: false,
+            data: None,
+            ft: None,
         }
     }
 
     // メッセージごとに呼び出されます
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::AddOne => self.value += 1
+            Msg::AddOne =>{ self.value += 1}
+            Msg::FetchData => {
+                self.fetching = true; // 4.
+
+                let callback = self.link.send_message(
+                    move |response: Response<Json<Result<Item, Error>>>| { // 2.
+                        let (meta, Json(data)) = response.into_parts();
+                        if meta.status.is_success() {
+                            Msg::FetchReady(data)
+                        } else {
+                            Msg::Ignore
+                        }
+                    },
+                );
+                let request = Request::get("/data.json").body(Nothing).unwrap(); // 5.
+
+                let task = FetchService::fetch(request, callback);
+            }
+            Msg::FetchReady(response) => {
+                self.fetching = false; // 4.
+                self.data = response.map(|data| data.value).ok(); // 6.
+            }
+            Msg::Ignore => {
+                return false;
+            }
         }
         true
     }
@@ -59,6 +103,28 @@ impl Component for Model {
 // wasm module が読み込まれたときのエントリポイント
 #[wasm_bindgen(start)]
 pub fn run_app() {
+    wasm_logger::init(wasm_logger::Config::default());
     // body タグにSPAをマウント
     App::<Model>::new().mount_to_body();
 }
+
+
+
+
+
+
+
+
+
+// let get_request = Request::get("https://receipten-backend.ojisan.vercel.app/api/get-items?id=JtvoNq7CnSUU6HvB1QPK")
+// .body(Nothing)
+// .expect("Could not build that request");
+// info!("Update: {:?}", get_request);
+
+// let callback = link.callback(|response: Response<Result<String, Error>>| {
+// if response.status().is_success() {
+//     Msg::Noop
+// } else {
+//     Msg::Error
+// }
+// })
